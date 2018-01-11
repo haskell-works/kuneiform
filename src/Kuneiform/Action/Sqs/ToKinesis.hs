10331483eb14078e1afead5063eff6ece01450c5
John Ky <newhoggy@gmail.com>
2018-01-11 18:43:47 +1100

    Add kinesisPutKV function

--- src/Kuneiform/Action/Sqs/ToKinesis.hs
+++ src/Kuneiform/Action/Sqs/ToKinesis.hs
@@ -3,8 +3,24 @@
 module Kuneiform.Action.Sqs.ToKinesis where
 
 import Control.Lens
+import Control.Monad.IO.Class
+import Data.Conduit
 import Data.Monoid
+import Data.Text
+import Kuneiform.Aws.Core
 import Kuneiform.Option.Cmd.Sqs.ToKinesis
+import Network.AWS.Data.ByteString
+import Network.AWS.Data.Text
+import Network.AWS.Kinesis
 
 actionSqsToKinesis :: CmdSqsToKinesis -> IO ()
 actionSqsToKinesis opts = putStrLn $ "Streaming from " <> (opts ^. src) <> " to " <> (opts ^. tgt)
+
+kinesisPutKV :: (MonadIO m, ToText k, ToByteString v) => Text -> Conduit (k, v) m PutRecordResponse
+kinesisPutKV streamName = do
+  ma <- await
+  case ma of
+    Just (k, v) -> do
+      resp <- liftIO $ sendAws $ putRecord streamName (toBS v) (toText k)
+      yield resp
+    Nothing         -> return ()
---
+++ src/Kuneiform/Aws/Core.hs
@@ -0,0 +1,14 @@
+module Kuneiform.Aws.Core
+  ( sendAws
+  ) where
+
+import Control.Lens
+import Control.Monad.IO.Class
+import Control.Monad.Trans.AWS hiding (await)
+import System.IO
+
+sendAws :: (MonadIO m, AWSRequest a) => a -> m (Rs a)
+sendAws r = do
+  lgr <- newLogger Trace stdout
+  env <- liftIO $ newEnv Discover <&> set envLogger lgr . set envRegion Oregon
+  liftIO $ runResourceT . runAWST env $ send r
