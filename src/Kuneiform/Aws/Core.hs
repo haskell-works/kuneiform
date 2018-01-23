e294f73d482cc0947174ca7dddfce919400144a8
John Ky <newhoggy@gmail.com>
2018-01-24 07:56:52 +1100

    Change log level

--- src/Kuneiform/Aws/Core.hs
+++ src/Kuneiform/Aws/Core.hs
@@ -9,6 +9,6 @@ import System.IO
 
 sendAws :: (MonadIO m, AWSRequest a) => a -> m (Rs a)
 sendAws r = do
-  lgr <- newLogger Trace stdout
+  lgr <- newLogger Error stdout
   env <- liftIO $ newEnv Discover <&> set envLogger lgr . set envRegion Oregon
   liftIO $ runResourceT . runAWST env $ send r
