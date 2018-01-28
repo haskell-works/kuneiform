43ea910053621aa3a5c288f9dd9c13f573c64dff
John Ky <newhoggy@gmail.com>
2018-01-28 19:27:45 +1100

    New boundedChanSource and boundedChanSink functions

---
+++ src/Kuneiform/Conduit/BoundedChan.hs
@@ -0,0 +1,31 @@
+module Kuneiform.Conduit.BoundedChan where
+
+import Conduit
+import Control.Concurrent.BoundedChan
+import Control.Lens
+import Control.Monad
+import Data.Text
+import Kuneiform.Aws.Core
+import Kuneiform.Envelope
+import Network.AWS.SQS
+
+boundedChanSource :: MonadIO m => BoundedChan (Maybe a) -> Source m a
+boundedChanSource c = do
+  ma <- liftIO (readChan c)
+  case ma of
+    Just a -> do
+      yield a
+      boundedChanSource c
+    Nothing -> return ()
+
+boundedChanSink :: MonadIO m => BoundedChan (Maybe a) -> Sink a m ()
+boundedChanSink c = do
+  ma <- await
+  case ma of
+    Just a -> do
+      liftIO $ writeChan c (Just a)
+      return ()
+      boundedChanSink c
+    Nothing -> do
+      liftIO $ writeChan c Nothing
+      return ()
