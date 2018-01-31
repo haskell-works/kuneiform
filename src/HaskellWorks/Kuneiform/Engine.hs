be0bbf8fd0db2b8ebe653e941bfb89352dbf037c
John Ky <newhoggy@gmail.com>
2018-01-31 21:53:32 +1100

    Stuff

--- src/HaskellWorks/Kuneiform/Engine.hs
+++ src/HaskellWorks/Kuneiform/Engine.hs
@@ -1,75 +1,31 @@
 
-{-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE GADTs               #-}
-{-# LANGUAGE KindSignatures      #-}
-{-# LANGUAGE LambdaCase          #-}
-{-# LANGUAGE RankNTypes          #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell     #-}
-{-# LANGUAGE TypeFamilies        #-}
-
 module HaskellWorks.Kuneiform.Engine where
 
-import Data.Kind
-import Data.Singletons
-import Data.Singletons.TH
+import qualified Data.Map as M
 
-data ResourceType
-  = RtS3Bucket
-  | RtSqs
-  | RtSns
-  deriving (Show, Eq)
+data ResS3Bucket = ResS3Bucket
+  { resS3BucketName       :: String
+  , resS3BucketActualName :: String
+  } deriving (Eq, Show)
 
-genSingletons [''ResourceType]
+data ResSqs = ResSqs
+  { resSqsName       :: String
+  , resSqsActualName :: String
+  } deriving (Eq, Show)
 
-data Door :: ResourceType -> Type where
-    UnsafeMkDoor :: { doorMaterial :: String } -> Door s
+data ResSns = ResSns
+  { resSnsName       :: String
+  , resSnsActualName :: String
+  } deriving (Eq, Show)
 
-closeDoor :: Door 'RtS3Bucket -> Door 'RtSqs
-closeDoor (UnsafeMkDoor m) = UnsafeMkDoor m
+data ResSnsSubscription = ResSnsSubscription
+  { resSnsSubscriptionName       :: String
+  , resSnsSubscriptionActualName :: String
+  } deriving (Eq, Show)
 
-lockDoor :: Door 'RtSqs -> Door 'RtSns
-lockDoor (UnsafeMkDoor m) = UnsafeMkDoor m
-
-openDoor :: Door 'RtSqs -> Door 'RtS3Bucket
-openDoor (UnsafeMkDoor m) = UnsafeMkDoor m
-
-doorStatus :: Sing s -> Door s -> ResourceType
-doorStatus SRtS3Bucket _ = RtS3Bucket
-doorStatus SRtSqs _      = RtSqs
-doorStatus SRtSns _      = RtSns
-
-lockAnyDoor :: Sing s -> (Door s -> Door 'RtSns)
-lockAnyDoor = \case
-    SRtS3Bucket -> lockDoor . closeDoor
-    SRtSqs      -> lockDoor
-    SRtSns      -> id
-
-doorStatus_ :: SingI s => Door s -> ResourceType
-doorStatus_ = doorStatus sing
-
-lockAnyDoor_ :: SingI s => Door s -> Door 'RtSns
-lockAnyDoor_ = lockAnyDoor sing
-
-mkDoor :: Sing s -> String -> Door s
-mkDoor _ = UnsafeMkDoor
-
-main :: IO ()
-main = return ()
-
-
--- Exercises
-
-unlockDoor :: Int -> Door 'RtSns -> Maybe (Door 'RtSqs)
-unlockDoor n (UnsafeMkDoor m)
-    | n `mod` 2 == 1 = Just (UnsafeMkDoor m)
-    | otherwise      = Nothing
-
-openAnyDoor :: SingI s => Int -> Door s -> Maybe (Door 'RtS3Bucket)
-openAnyDoor n = openAnyDoor_ sing
-  where
-    openAnyDoor_ :: Sing s -> Door s -> Maybe (Door 'RtS3Bucket)
-    openAnyDoor_ = \case
-      SRtS3Bucket -> Just
-      SRtSqs      -> Just . openDoor
-      SRtSns      -> fmap openDoor . unlockDoor n
+data ResourceTypes = ResourceTypes
+  { rtsS3Buckets       :: M.Map String ResS3Bucket
+  , rtsSqs             :: M.Map String ResSqs
+  , rtsSns             :: M.Map String ResSns
+  , rtsSnsSubscription :: M.Map String ResSnsSubscription
+  } deriving (Eq, Show)
